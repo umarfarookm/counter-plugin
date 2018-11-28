0519294dd86090fde0c2b39a0cd032a1ae4aae4e
umarfarookm <umarfarook89@gmail.com>
2018-11-28 20:44:32 +0530

    added readme content

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-#
+#Number Spinner
+
+*Simple html number spinner*
+
+In HTML5, you can have a number input field as a spinner which you have up and down arrow at the right of the textbox to increase or decrease the number value. 
+Please see the styled spinner component as below, 
+
+![](spinner.png)
+
+#Install
+
+`$ npm install html-number-spinner`
+
+#Usage
+
+*Step 1:*
+
+    <div class="counter"></div>
+    
+*Step 2:*
+
+    $('.counter').htmlNumberSpinner()
+    
+###Keywords
+
+    html-number-spinner
--- plugin.js
+++ plugin.js
@@ -5,6 +5,18 @@
         /*creating the counter buttons*/
         $(this).append("<div class='btn decrementer'>-</div> <div class='btn display-out'></div> <div class='btn incrementer'>+</div>");
 
+        /*styling the component*/
+        /*styling the counter buttons*/
+        $(this).find('.btn').css({"float":"left", "width":"50px", "height":"30px", "font-size":"25px", "text-align":"center", "vertical-align":"middle", "line-height":"1", "cursor":"pointer", "user-select":"none"});
+        $(this).find('.incrementer').css({"background-color":"slateblue", "color":"white", "border": "1px solid slateblue"});
+        $(this).find('.decrementer').css({"background-color":"hotpink", "color":"white", "font-size":"25px", "border": "1px solid hotpink"});
+        $(this).find('.display-out').css({"background-color":"white", "border": "1px solid","color":"black",
+            "line-height":"normal",
+            "border-left-color": "hotpink",
+            "border-right-color": "slateblue",
+            "border-top-color": "lightblue",
+            "border-bottom-color": "lightblue"});
+
         $(this).find('.display-out').text(inputValue);
 
         $(this).find('.incrementer').click(function () {
