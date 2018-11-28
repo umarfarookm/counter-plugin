596a6481f6b8c7b787e1503f6c5447564298ae33
umarfarookm <umarfarook89@gmail.com>
2018-11-28 18:29:55 +0530

    wrap the plugin in self invoking

--- plugin.js
+++ plugin.js
@@ -1,22 +1,24 @@
-$.fn.myCounter = function(){
-    var inputValue = 0;
+(function($) {
+    $.fn.myCounter = function () {
+        var inputValue = 0;
 
-    /*creating the counter buttons*/
-    $(this).append("<div class='btn decrementer'>-</div> <div class='btn display-out'></div> <div class='btn incrementer'>+</div>");
+        /*creating the counter buttons*/
+        $(this).append("<div class='btn decrementer'>-</div> <div class='btn display-out'></div> <div class='btn incrementer'>+</div>");
 
-    $(this).find('.display-out').text(inputValue);
+        $(this).find('.display-out').text(inputValue);
 
-    $(this).find('.incrementer').click(function () {
-        inputValue++;
-        var parentEl = $(this).parent();
-        parentEl.find('.display-out').text(inputValue);
-    });
+        $(this).find('.incrementer').click(function () {
+            inputValue++;
+            var parentEl = $(this).parent();
+            parentEl.find('.display-out').text(inputValue);
+        });
 
-    $(this).find('.decrementer').click(function () {
-        inputValue--;
-        var parentEl = $(this).parent();
-        parentEl.find('.display-out').text(inputValue);
-    })
-};
+        $(this).find('.decrementer').click(function () {
+            inputValue--;
+            var parentEl = $(this).parent();
+            parentEl.find('.display-out').text(inputValue);
+        })
+    }
+}(jQuery));
 
 $( "#umar-counter" ).myCounter();
